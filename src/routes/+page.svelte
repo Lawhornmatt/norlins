7a5563385c85ffd6219e7ed3f57f6dd9d6056e90
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2024-09-17 11:42:53 -0500

    Reset State
    When navigating to the homepage, all state variables are reset. Players replaying or re-navigating back to the begining start with clean slate for another round.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,13 @@
 	import markdownit from 'markdown-it';
 	const md = markdownit();
 	import storytext from '../../content/A1_Intro.md?raw';
+
+	import { drunkness } from '$lib/drunkness';
+	import { boozelist } from '$lib/boozelist';
+	import { drinkstate } from '$lib/drinkstate';
+	$drunkness = 0;
+	$boozelist = '';
+	$drinkstate = false;
 </script>
 
 <div class="card flex flex-col justify-around p-4 w-full h-96 lg:h-full mb-5 lg:m-3">
